4218fd91d5808117f73276f9d9413fc3431114b6
Caio <cjlcarvalho@live.com>
2017-12-15 16:27:51 -0300

    Updating snow plugin

--- snow/snowimagefactory.cpp
+++ snow/snowimagefactory.cpp
@@ -4,3 +4,19 @@
 SnowImageFactory::SnowImageFactory()
 {
 }
+
+IBackgroundImage* SnowImageFactory::createBackgroundImage(){
+    return nullptr;
+}
+
+IGroundImage* SnowImageFactory::createGroundImage(){
+    return nullptr;
+}
+
+IPlayerImage* SnowImageFactory::createPlayerImage(){
+    return nullptr;
+}
+
+IBlockImage* SnowImageFactory::createBlockImage(){
+    return nullptr;
+}
--- snow/snowimagefactory.h
+++ snow/snowimagefactory.h
@@ -1,13 +1,20 @@
 #ifndef SNOWIMAGEFACTORY_H
 #define SNOWIMAGEFACTORY_H
 
+#include <QtPlugin>
 #include "snow_global.h"
+#include "../blok-images/imagefactory.h"
 
-class SNOWSHARED_EXPORT SnowImageFactory
+class SNOWSHARED_EXPORT SnowImageFactory : public ImageFactory
 {
-
+    Q_OBJECT
+    Q_INTERFACES(ImageFactory)
 public:
     SnowImageFactory();
+    virtual IBackgroundImage* createBackgroundImage();
+    virtual IGroundImage* createGroundImage();
+    virtual IPlayerImage* createPlayerImage();
+    virtual IBlockImage* createBlockImage();
 };
 
 #endif // SNOWIMAGEFACTORY_H
