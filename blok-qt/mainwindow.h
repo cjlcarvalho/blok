5d8316a4ae1e2d4b49c3705dd2182541e1053138
Caio Carvalho <caiojcarvalho@gmail.com>
2018-01-21 00:44:04 -0300

    Including mainwindow copyright

--- blok-qt/mainwindow.h
+++ blok-qt/mainwindow.h
@@ -1,5 +1,6 @@
 /********************************************************************************
  *   Copyright (C) 2012 by Sandro Andrade <sandroandrade@kde.org>               *
+ *   Copyright (C) 2018 by Caio Carvalho <caiojcarvalho@gmail.com>              *
  *                                                                              *
  *   This program is free software; you can redistribute it and/or modify       *
  *   it under the terms of the GNU General Public License as published by       *
